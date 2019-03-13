9fe291091cf49932720471bfc4a46aac1f6ce7a5
Hüseyin Biçen <huseynbicen@outlook.com>
2019-03-14 01:10:53 +0300

    Master

--- ChosenDropDownListMVC/App_Start/RouteConfig.cs
+++ ChosenDropDownListMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ChosenDropDownListMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Payments", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ChosenDropDownListMVC/Controllers/PaymentsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChosenDropDownListMVC.Models;
+
+namespace ChosenDropDownListMVC.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private EmployeeDBEntities db = new EmployeeDBEntities();
+
+        // GET: Payments
+        public ActionResult Index()
+        {
+            var payments = db.Payments.Include(p => p.Employee);
+            return View(payments.ToList());
+        }
+
+        // GET: Payments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public ActionResult Create()
+        {
+            var clients = db.Employees.Select(s => new
+            {
+                Text = s.Name + " | " + s.Office + " | " + s.Position,
+                Value = s.EmployeeID
+            }).ToList();
+            ViewBag.Employee_Id = new SelectList(clients, "Value", "Text");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Employee_Id,Date,Total")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Employee_Id = new SelectList(db.Employees, "EmployeeID", "Name", payment.Employee_Id);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            var clients = db.Employees.Select(s => new
+            {
+                Text = s.Name + " | " + s.Office + " | " + s.Position,
+                Value = s.EmployeeID
+            }).ToList();
+            ViewBag.Employee_Id = new SelectList(clients, "Value", "Text", payment.Employee_Id);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Employee_Id,Date,Total")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Employee_Id = new SelectList(db.Employees, "EmployeeID", "Name", payment.Employee_Id);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payment payment = db.Payments.Find(id);
+            db.Payments.Remove(payment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
